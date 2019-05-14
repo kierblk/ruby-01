7ec7a13bd773aa0375cbdb95c61b42cd90c51845
Sara McCombs <theSaraMcCombs@gmail.com>
2019-05-14 14:26:57 -0400

    completed exercise, working on study drill 2

--- ex12.rb
+++ ex12.rb
@@ -1,2 +1,24 @@
 # https://learnrubythehardway.org/book/
 # page 60, exercise 12
+
+print "Give me a number: "
+number = gets.chomp.to_i
+
+bigger = number * 100
+puts "A bigger number is #{bigger}."
+
+print "Give me another number: "
+another = gets.chomp
+number = another.to_i
+
+smaller = number / 100
+puts "A smaller number is #{smaller}."
+
+# Study Drill, number 2
+
+print "Give me a dollar amount: "
+amount = gets.chomp.to_f
+
+ten_percent = amount * 0.1
+
+puts "Ten percent is #{ten_percent}."
