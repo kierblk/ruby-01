4315892f6563955e7354a8f85bd97cdf8286a662
Sara McCombs <theSaraMcCombs@gmail.com>
2019-05-14 14:07:07 -0400

    completed exercise

---
+++ ex11.rb
@@ -0,0 +1,11 @@
+# https://learnrubythehardway.org/book/
+# page 58, exercise 11
+
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall, and #{weight} heavy."
