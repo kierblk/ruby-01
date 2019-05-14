d34922b01fca80d6e620941ec8cb7a283f2f292c
Sara McCombs <theSaraMcCombs@gmail.com>
2019-05-13 21:24:07 -0400

    initial commit

---
+++ ex5.rb
@@ -0,0 +1,16 @@
+my_name = 'Sara C. McCombs'
+my_age = 36
+my_height = 74 # inches
+my_weight = 185 # pounds
+my_eyes = 'Hazel'
+my_teeth = 'white'
+my_hair = 'Red'
+
+puts "Let's talk about #{my_name}"
+puts "She's #{my_height} inches tall."
+puts "She's #{my_weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "She's got #{my_eyes} eyes and #{my_hair} hair."
+puts "Her teeth are usually #{my_teeth} depending on the coffee."
+
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}"
---
+++ ex7.rb
@@ -0,0 +1,4 @@
+puts "Mary had a little lamb."
+puts "Its fleece was white as #{'snow'}"
+puts "And everywhere that MAry went."
+puts
