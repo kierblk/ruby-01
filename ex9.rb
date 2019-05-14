3757b01eb70c6aede1cc3a8b1394bf0027b10ec6
Sara McCombs <theSaraMcCombs@gmail.com>
2019-05-13 22:12:31 -0400

    added comment

--- ex9.rb
+++ ex9.rb
@@ -2,6 +2,7 @@
 # page 52, exercise 9
 
 days = "Mon Tue Wed Thu Fri Sat Sun"
+# This \ (backslash) character encodes difficult-to-type characters into a string. There are various ”escape sequences” available for different characters you might want to use.
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
 puts "Here are the days: #{days}"
