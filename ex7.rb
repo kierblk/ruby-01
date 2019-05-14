d0eaa421d860f40dab7423486d2ce848dec64853
Sara McCombs <theSaraMcCombs@gmail.com>
2019-05-13 21:39:19 -0400

    added comments

--- ex7.rb
+++ ex7.rb
@@ -1,6 +1,8 @@
 # https://learnrubythehardway.org/book/
 # page 48 - exercise 7
 puts "Mary had a little lamb."
+# The line below does not contain a variable named snow. the single quotes indicate it is a string with the word snow in it. a variable wouldn't have the single-quotes around it.
+# In Ruby the " (double-quote) tells Ruby to replace variables it finds with #{}, but the ' (single-quote) tells Ruby to leave the string alone and ignore any variables inside it.
 puts "Its fleece was white as #{'snow'}"
 puts "And everywhere that Mary went."
 puts "." * 10 
